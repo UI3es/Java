c00bcb305400c1e194513ae1bd3e6a48b21853b2
UI3es <32736369+UI3es@users.noreply.github.com>
2018-06-03 17:36:08 +0500

    UI3es
    Uploaded "Lab_1"

---
+++ lab_1/Lab_1.java
@@ -0,0 +1,21 @@
+/*
+ *
+ * Output word "Hello World!"
+ *
+ */
+package lab_1;
+
+/**
+ *
+ * @author UI3es
+ */
+public class Lab_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+    
+}
